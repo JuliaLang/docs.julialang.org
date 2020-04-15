9a960ec9bf6e1c281785ccd6d80a34475f83b3c9
Fredrik Ekre <ekrefredrik@gmail.com>
2020-04-15 22:47:50 +0200

    Skip trying to deploy docs with Documenter.deploydocs.

--- pdf/make.jl
+++ pdf/make.jl
@@ -40,6 +40,7 @@ function makedocs(julia_exec)
     @sync begin
         builder = @async begin
             withenv("TRAVIS_REPO_SLUG" => nothing, # workaround Documenter bugs and julia#26314
+                    "DOCUMENTER_KEY" => nothing, # skips deploydocs with the BuildBotConfig (see doc/make.jl)
                     "BUILDROOT" => nothing) do
                 run(`make -C $(JULIA_SOURCE)/doc pdf texplatform=docker JULIA_EXECUTABLE=$(julia_exec)`)
             end
