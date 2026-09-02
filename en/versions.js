4786d19d9e6acb0ad8aaa56a1b377305ec189783
Documenter.jl <documenter@juliadocs.github.io>
2026-09-02 13:15:14 +0000

    build based on 8f33e09afea0fcd605e86bb784b24b56a812942a
    build link: https://buildkite.com/julialang/julia-publish/builds/374
    commit link: https://github.com/JuliaLang/julia/commit/8f33e09afea0fcd605e86bb784b24b56a812942a

--- en/versions.js
+++ en/versions.js
@@ -17,8 +17,8 @@ var DOC_VERSIONS = [
   "v0.5",
   "v0.4",
   "v0.3",
-  "v1.13.0-rc3",
-  "v1.14-dev",
+  "v1.13.0-rc4",
+  "v1.13-dev",
 ];
 var DOCUMENTER_NEWEST = "v1.12.7";
 var DOCUMENTER_STABLE = "v1.12";
